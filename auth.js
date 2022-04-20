930847788b8b88cac743e64f23422dea2e160681
Laksh Chakraborty <lakshc2010@gmail.com>
2022-04-20 02:29:20 -0400

    refactored backend

--- auth.js
+++ auth.js
@@ -1,57 +1,75 @@
-const mongoose = require('mongoose');
-const passport = require('passport');
-const { secret } = require('./config');
-const LocalStrategy = require('passport-local').Strategy;
-const JWTstrategy = require('passport-jwt').Strategy;
-const ExtractJWT = require('passport-jwt').ExtractJwt;
-const User = mongoose.model('User');
+const mongoose = require("mongoose");
+const passport = require("passport");
+const { secret } = require("./config");
+const LocalStrategy = require("passport-local").Strategy;
+const JWTstrategy = require("passport-jwt").Strategy;
+const ExtractJWT = require("passport-jwt").ExtractJwt;
+const User = mongoose.model("User");
 
-passport.use("register", new LocalStrategy({
-    usernameField: "email",
-    passwordField: "password"
-}, async (email, password, done) => {
-    try {
-        const user = await User.create({email, password, lists: [], people: [], timelines: []});
-        return done(null, user)
-    } catch (e) {
-        done(e);
-    }
-}));
-
-passport.use("login", new LocalStrategy({
-    usernameField: "email",
-    passwordField: "password"
-}, async (email, password, done) => {
-    try {
-        const user = await User.findOne({email});
-        if (!user) {
-            return done(null, false, {message: "User not found"});
+passport.use(
+    "register",
+    new LocalStrategy(
+        {
+            usernameField: "email",
+            passwordField: "password",
+        },
+        async (email, password, done) => {
+            try {
+                const user = await User.create({
+                    email,
+                    password,
+                    lists: [],
+                    people: [],
+                    timelines: [],
+                });
+                return done(null, user);
+            } catch (e) {
+                done(e);
+            }
         }
+    )
+);
 
-        const validate = await user.isValidPassword(password);
+passport.use(
+    "login",
+    new LocalStrategy(
+        {
+            usernameField: "email",
+            passwordField: "password",
+        },
+        async (email, password, done) => {
+            try {
+                const user = await User.findOne({ email });
+                if (!user) {
+                    return done(null, false, { message: "User not found" });
+                }
 
-        if (!validate) {
-            return done(null, false, {message: "Wrong password"});
+                const validate = await user.isValidPassword(password);
+
+                if (!validate) {
+                    return done(null, false, { message: "Wrong password" });
+                }
+
+                return done(null, user, { message: "Logged in successfully" });
+            } catch (e) {
+                done(e);
+            }
         }
-
-        return done(null, user, {message: "Logged in successfully"});
-    } catch(e) {
-        done(e);
-    } 
-}));
+    )
+);
 
 passport.use(
     new JWTstrategy(
-      {
-        secretOrKey: secret,
-        jwtFromRequest: ExtractJWT.fromUrlQueryParameter('secret_token')
-      },
-      async (token, done) => {
-        try {
-          return done(null, token.user);
-        } catch (error) {
-          done(error);
+        {
+            secretOrKey: secret,
+            jwtFromRequest: ExtractJWT.fromUrlQueryParameter("secret_token"),
+        },
+        async (token, done) => {
+            try {
+                return done(null, token.user);
+            } catch (error) {
+                done(error);
+            }
         }
-      }
     )
 );
--- config.js
+++ config.js
@@ -2,5 +2,5 @@ require('dotenv').config();
 
 module.exports = {
   secret: process.env.JWT_KEY,
-  mongoUrl: process.env.MONGO_URL
+  mongoUrl: process.env.MONGODB_URI
 };
